9ad76ef84059f1c84f8e81fca499e8bbf207595f
dkoslicki <dmkoslicki@gmail.com>
2016-02-22 14:22:23 -0800

    removed uneaded formatting requirement

--- src/MaxEntMatrix.py
+++ src/MaxEntMatrix.py
@@ -66,7 +66,7 @@ def calc_max_ent(file_path_c, file_path_r, file_path_output):
 			Z[i,j] = x[i]*y[j]/(1+x[i]*y[j])
 	
 	
-	np.savetxt(file_path_output, Z, delimiter=',', fmt='%.10f')
+	np.savetxt(file_path_output, Z, delimiter=',')
 
 
 	
